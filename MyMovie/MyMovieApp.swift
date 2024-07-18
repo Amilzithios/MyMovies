eecb7c743737131e4b66ac07d56ba069f1e62082
Amilzithios <amilzithios@gmail.com>
2024-07-18 19:51:35 +0530

    Initial Commit

---
+++ MyMovie/MyMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieApp.swift
+//  MyMovie
+//
+//  Created by Amilzith on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
